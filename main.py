aa6c75065221bda9f3ebecabae8a2762d93e09f7
Thirasak arrream <thirasak2545ton@gmail.com>
2024-11-24 13:51:59 +0700

    Add frontend integration

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+# เสิร์ฟไฟล์ Static (JS, CSS, รูปภาพ ฯลฯ)
+app.mount("/static", StaticFiles(directory="frontend/assets"), name="static")
+
+# Route สำหรับเสิร์ฟ index.html
 @app.get("/")
-def read_root():
+async def serve_frontend():
+    return FileResponse("frontend/index.html")
+
+# ตัวอย่าง API
+@app.get("/api")
+async def read_root():
     return {"Hello": "World"}
